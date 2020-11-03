0479903399d47c75381e58563dd08112521ac253
Kevin Velasquez <kvelasq1@ugradx.cs.jhu.edu>
2020-11-03 15:03:00 -0500

    Fetch all files ending with .so

--- driver.cpp
+++ driver.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <sys/types.h>
+#include <dirent.h>
 using namespace std;
 
 string fetch_dir() {
@@ -7,6 +10,27 @@ string fetch_dir() {
     return (dir != NULL) ? dir : "plugins";
 }
 
+vector<string> fetch_files(string plugin_dir) {
+    vector<string> files;
+    DIR* dir;
+    struct dirent* dirp;
+    // Return if we cannot read from directory
+    if ((dir = opendir(plugin_dir.c_str())) == NULL) {
+        cout << "Error " << errno << " opening " << plugin_dir << endl;
+        return files;
+    }
+    // Iterate through all files and push back those with ".so"
+    while ((dirp = readdir(dir)) != NULL) {
+        string file = dirp->d_name;
+        if (file.find(".so") != string::npos) {
+            files.push_back(file);
+        }
+    }
+    return files;
+}
+
 int main(int argc, char* argv[]) {
     string plugin_dir = fetch_dir();
+    vector<string> plugin_files = fetch_files(plugin_dir);
+    return 0;
 }
