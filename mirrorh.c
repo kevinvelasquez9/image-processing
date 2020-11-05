c03781e1757b88d9e25628a1ff43e3b2f777ccfc
Christian Helgeson <cmhhelgeson@gmail.com>
2020-11-05 16:41:49 -0500

    Added other image plugins

---
+++ mirrorh.c
@@ -0,0 +1,53 @@
+//
+// Example plugin: it just swaps the blue and green color component
+// values for each pixel in the source image.
+//
+
+#include <stdlib.h>
+#include "image_plugin.h"
+
+struct Arguments {
+	// This plugin doesn't accept any command line arguments;
+	// just define a single dummy field.
+	int dummy;
+};
+
+const char *get_plugin_name(void) {
+	return "mirrorh";
+}
+
+const char *get_plugin_desc(void) {
+	return "mirror image horizontally";
+}
+
+void *parse_arguments(int num_args, char *args[]) {
+	(void) args; // this is just to avoid a warning about an unused parameter
+
+	if (num_args != 0) {
+		return NULL;
+	}
+	return calloc(1, sizeof(struct Arguments));
+}
+
+struct Image *transform_image(struct Image *source, void *arg_data) {
+	struct Arguments *args = arg_data;
+
+	// Allocate a result Image
+	struct Image *out = img_create(source->width, source->height);
+	if (!out) {
+		free(args);
+		return NULL;
+	}
+
+	for (unsigned i = 0; i < source->height; i++) {
+        uint32_t curRow = i * source->width;
+        uint32_t endRow = ((i + 1) * source->width) - 1;
+        for (unsigned j = 0; j < source->width/2; j++) {
+            out[curRow + j] = source[endRow- j];
+        }
+	}
+
+	free(args);
+
+	return out;
+}
